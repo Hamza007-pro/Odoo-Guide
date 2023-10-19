a48fe5e7ec91a737ccdb2be27ad59a1e41b8141a
Hamza BENRAHHOU <46993247+Hamza007-pro@users.noreply.github.com>
2023-10-19 15:35:52 -0700

    Update BlocSnippet.jsx

--- app/Components/BlocSnippet.jsx
+++ app/Components/BlocSnippet.jsx
@@ -7,7 +7,7 @@ const BlocSnippet = ({ commands }) => {
     <div onClick={() => navigator.clipboard.writeText(commands.join("\n"))} className="bg-slate-800 min-h-[100px]  rounded-lg p-4 relative">
       <div className="p-2 bg-primary flex absolute right-4 top-4 rounded-lg gap-2 drop-shadow-lg border-white border cursor-pointer hover:bg-emerald-600 active:bg-emerald-900 focus:outline-none focus:ring focus:ring-emerald-300">
         <span>Copy</span>
-        <Image src={"/images/copy.svg"} width={18} height={18}></Image>
+        <Image src={"https://hamza007-pro.github.io/Odoo-Guide/images/copy.svg"} width={18} height={18}></Image>
       </div>
       {commands.map((cmd, index) => (
         <div key={index}>
