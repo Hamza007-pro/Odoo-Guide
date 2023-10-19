fce5dd45a7026257c9c79d70da3da21f5f39cd74
Hamza BENRAHHOU <46993247+Hamza007-pro@users.noreply.github.com>
2023-10-19 15:31:52 -0700

    Update page.jsx

--- app/windows/page.jsx
+++ app/windows/page.jsx
@@ -8,55 +8,55 @@ const Page = () => {
         {
             title: "1. Execute the downloaded file",
             content:"This step involves running the downloaded Odoo installation file to kickstart the setup. By executing this file, you initiate the installation wizard, guiding you through critical configuration steps.",
-            imagePath: "/images/1.png",
+            imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/1.png",
           },
         {
           title: "2. Installation setup",
           content:
             "Please proceed by selecting the 'Next' option within the setup window to advance the installation.",
-          imagePath: "/images/2.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/2.png",
         },
         {
           title: "3. Terms and conditions",
           content:
             "Subsequently, you will be presented with the software's terms and conditions. To continue with the installation, please click on the 'Agree' or 'Accept' button, depending on the terminology used by the software.",
-          imagePath: "/images/3.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/3.png",
         },
         {
           title: "4. Server configuration",
           content:
             "Within the installation options, please select 'Odoo Server with SQL PostgreSQL Server' as your preferred installation type.",
-          imagePath: "/images/4.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/4.png",
         },
         {
           title: "5. Install location",
           content:
             "Next, you'll be prompted to specify the destination path where Odoo will be installed. Please choose a directory or location on your system for the Odoo installation, ensuring it aligns with your preferences and organizational requirements.",
-          imagePath: "/images/5.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/5.png",
         },
         {
           title: "6. Wait for installation",
           content:
             "Once you have selected the installation path, please initiate the installation process. Kindly note that this installation may take some time to complete. Your patience during this phase is greatly appreciated.",
-          imagePath: "/images/6.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/6.png",
         },
         {
           title: "7. Installation completed",
           content:
             "Once the installation process has successfully concluded, kindly proceed by clicking on the 'Next' button to continue with the setup.",
-          imagePath: "/images/7.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/7.png",
         },
         {
           title: "8. Close setup",
           content:
             "To finalize and complete the installation, please click on the 'Finish' button.",
-          imagePath: "/images/8.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/8.png",
         },
         {
           title: "9. Access and configuration",
           content:
             "To access Odoo and begin the database configuration and admin setup, please open your web browser and navigate to the following link: http://localhost:8069 From there, follow the on-screen instructions to configure the database. Remember to save the master password for future use. Then, proceed to enter essential details such as the database name, user information (email, password, phone number), select your preferred language and country, and finally, click 'Submit' to finalize the Odoo installation process successfully.",
-          imagePath: "/images/9.png",
+          imagePath: "https://hamza007-pro.github.io/Odoo-Guide/images/9.png",
         },
       ];
 
